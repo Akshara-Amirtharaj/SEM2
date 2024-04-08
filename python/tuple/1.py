448919b59e182b19fef7bce8800a07d48e08f34f
Akshara-Amirtharaj <157888145+Akshara-Amirtharaj@users.noreply.github.com>
2024-04-08 20:18:34 +0530

    Delete python/tuple directory

--- python/tuple/1.py
+++
@@ -1,9 +0,0 @@
-# lst=[(1,2),(2,3),(3,4)]
-# lst2=[]
-# for i in lst:
-#     lst2.append(sum(i))
-# print(lst2)
-
-# lst2 = [sum(i) for i in eval(input("Enter: "))]
-
-print([sum(i) for i in eval(input("Enter: "))])
