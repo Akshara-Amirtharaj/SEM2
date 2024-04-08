02e35ce68aefbb81a41fe9f17ef6acab5f6a3c10
Akshara-Amirtharaj <157888145+Akshara-Amirtharaj@users.noreply.github.com>
2024-04-08 20:19:01 +0530

    Delete python/class/list directory

--- python/class/list/2.py
+++
@@ -1,7 +0,0 @@
-M=int(input("Enter the total sum:"))
-n=eval(input("Enter the flavours:"))
-length=len(n)
-for i in range(0,length-1):
-    for j in range(0,i):
-        if n[i]+n[j]==M:
-            print(i,j)
